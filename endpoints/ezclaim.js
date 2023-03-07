99efc7bb7945cf5cfb6a349e0f9ee888a7679a06
Derek Blasko <derek@prosolaradvisors.com>
2023-03-07 12:46:49 -0500

    Added comments to exclaim endpoint

--- endpoints/ezclaim.js
+++ endpoints/ezclaim.js
@@ -1,22 +1,25 @@
+
+// Import necessary modules
 const { default: axios } = require("axios");
 require("dotenv").config({
     path: ".env",
 });
-const sign = require("./sign")
-
-
-
-
+const sign = require("./sign");
 
 async function claimEZ(uuid, issuer, tid) {
-	const { data: { data: { response: { account }, application: { issued_user_token } } } } = await axios.get(`http://localhost:5000/payload/${uuid}`);
-    const { data: { data: { offers } } } = await axios.get(`https://api.xrpldata.com/api/v1/xls20-nfts/offers/offerowner/${issuer}`)
-    const claims = offers.find(offer => offer.Destination === account && item.NFTokenID === tid);
-    if (!claims) {
-        return "No Offers"
+    // Fetch user's XRP Ledger account and XUMM issued token from local API
+    const { data: { data: { response: { account }, application: { issued_user_token } } } } = await axios.get(`http://localhost:5000/payload/${uuid}`);
+    // Fetch all active NFT sell offers on XRP Ledger for the issuer
+    const { data: { data: { offers } } } = await axios.get(`https://api.xrpldata.com/api/v1/xls20-nfts/offers/offerowner/${issuer}`);
+    // Find the NFT sell offer made to the user with specified token ID
+    const claimOffer = offers.find(offer => offer.Destination === account && item.NFTokenID === tid);
+    if (!claimOffer) {
+        // Return if no NFT sell offer found for the user with specified token ID
+        return "No Offers";
     }
-    const signQR = await sign(account, claims.OfferID, issued_user_token);
-    return signQR
+    // Create XUMM sign request for the user to claim the NFT sell offer
+    const signQR = await sign(account, claimOffer.OfferID, issued_user_token);
+    return signQR;
 };
 
 module.exports = claimEZ;
